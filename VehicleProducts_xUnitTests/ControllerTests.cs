e0760187e5b3fa5a6f80c680ffb2fa591fd06c59
freeburma <kophyoblue@gmail.com>
2022-02-05 14:48:45 +1300

    controller test remove

--- VehicleProducts_xUnitTests/ControllerTests.cs
+++ VehicleProducts_xUnitTests/ControllerTests.cs
@@ -16,74 +16,7 @@ namespace VehicleProducts_xUnitTests
 {
     public class ControllerTests
     {
-        /// <summary>
-        /// We need to create the Mock dependencies for controller. Read the StackOverflow article at the following link. 
-        /// Ref: https://stackoverflow.com/questions/43424095/how-to-unit-test-with-ilogger-in-asp-net-core
-        /// </summary>
+        
 
-
-        [Fact]
-        public async void TestController_Privacy_Test()
-        {
-
-            using (var db = new ProductDbContext(DatabaseService.TestDbContextOptions()))
-            {
-                //// Arrange 
-                await db.Vehicles.AddRangeAsync(DatabaseService.DummyMemoryTestList);
-                await db.SaveChangesAsync();
-
-                var mockILogger = Mock.Of<ILogger<HomeController>>();
-                var privacyController = new HomeController(mockILogger, db);
-
-                //// Act 
-                var result = privacyController.Privacy();
-                var getActionName = Assert.IsType<ViewResult>(result);
-
-                //// Assert 
-                Assert.Equal("Privacy", getActionName.ViewName);
-
-            }// end using 
-
-            
-
-
-        }// end TestController_Index_Test()
-
-        [Fact]
-        public async void HomeController_Index_Test()
-        {
-
-            using (var db = new ProductDbContext(DatabaseService.TestDbContextOptions()))
-            {
-                //// Arrange 
-                var mockILogger = Mock.Of<ILogger<HomeController>>();
-
-
-                await db.Vehicles.AddRangeAsync(DatabaseService.DummyMemoryTestList);
-                await db.SaveChangesAsync();
-
-                var homeController = new HomeController(mockILogger, db); // *** We need an empty constructor. We don't want to mess up with iLogger and DbContext. 
-
-                //// Act
-                var result = await homeController.Index();
-
-
-                //// Assert 
-                var viewResult = Assert.IsType<ViewResult>(result);
-                var vehicleList = Assert.IsAssignableFrom<IEnumerable<VehicleModel>>(viewResult.ViewData.Model); 
-                Assert.Equal(5, vehicleList.Count());
-
-            }// end using 
-
-
-
-
-
-
-
-
-        }// end HomeController_Index_Test()
-
-
-    }// end class PageModelTest
+    }// end class ControllerTests
 }
